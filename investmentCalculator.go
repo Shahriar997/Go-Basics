cb777476fe608e89380583581897227d7f67c947
Shahriar Hossain <66290875+Shahriar997@users.noreply.github.com>
2025-04-27 02:31:30 +0600

    add terminal input for variables with fmt.Scan()

--- investmentCalculator.go
+++ investmentCalculator.go
@@ -6,9 +6,18 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
+	var investmentAmount float64
 	expectedReturnRate := 5.5 // if using inferred type then do not need to use var.
-	var years float64 = 10
+	var years float64
+
+	fmt.Println("Please enter your investment amount: ")
+	fmt.Scan(&investmentAmount) // with & percent, we are sending the pointer or the address
+
+	fmt.Println("Please enter for how many year you want to invest: ")
+	fmt.Scan(&years)
+
+	fmt.Println("Please enter expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := calculate(
 		investmentAmount,
@@ -25,7 +34,7 @@ func calculate(
 	expectedReturnRate float64,
 	years float64,
 ) float64 {
-	inflationRate := 5.5
+	const inflationRate = 6.5
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
 
 	return futureValue / math.Pow(1 + inflationRate/100, years)
